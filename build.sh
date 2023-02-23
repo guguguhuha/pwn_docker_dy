eaaa888cfbc41afdfe215980ed5541915cf7512f
Your Name <you@example.com>
2023-02-23 06:58:20 +0000

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/sh
 export run_binary_cmd="./esp_learn"
 export image_name="test/pwn:esp_learn"
-export run_port=9999
 
 sudo sed -i "s%run_binary_cmd%$run_binary_cmd%g" ./ctf.xinetd
 
@@ -9,5 +8,5 @@ sudo docker build -t $image_name .
 
 unset run_binary_cmd
 
-# sudo docker run -d -p $run_port:9999 $image_name
+# sudo docker run -p 9999:9999 $image_name
 unset image_name
